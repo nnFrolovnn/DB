356edd65850a5ebe588600119686f49b3a444396
nnFrolovnn <mfi.ge.vlad@gmail.com>
2019-11-03 16:48:15 +0300

    lab_4

---
+++ Lab_4/lab4_var6_1.sql
@@ -0,0 +1,83 @@
+﻿use AdventureWorks2012;
+go
+
+drop table if exists Person.PhoneNumberTypeHst;
+go
+
+/*
+ *a) Создайте таблицу Person.PhoneNumberTypeHst, которая будет хранить 
+ *информацию об изменениях в таблице Person.PhoneNumberType.
+*/
+create table Person.PhoneNumberTypeHst (
+	ID int identity(1, 1) primary key,
+	Action char(6) not null check (Action IN('insert', 'update', 'delete')),
+	ModifiedDate datetime not null,
+	SourceID int not null,
+	UserName varchar(50) not null
+);
+go
+
+/*
+ *b) Создайте три AFTER триггера для трех операций INSERT, UPDATE, DELETE для таблицы Person.PhoneNumberType. 
+ *Каждый триггер должен заполнять таблицу Person.PhoneNumberTypeHst с указанием типа операции в поле Action.
+*/
+drop trigger if exists Person.person_Insert;
+go
+
+create trigger person_Insert on Person.PhoneNumberType
+after insert as
+	insert into Person.PhoneNumberTypeHst(Action, ModifiedDate, SourceID, UserName)
+	select 'insert', getdate(), inserted.PhoneNumberTypeID, CURRENT_USER
+	from inserted
+go
+
+drop trigger if exists Person.person_Update;
+go
+
+create trigger person_Update on Person.PhoneNumberType
+after update as
+	insert into Person.PhoneNumberTypeHst(Action, ModifiedDate, SourceID, UserName)
+	select 'update', getdate(), inserted.PhoneNumberTypeID, CURRENT_USER
+	from inserted
+go
+
+drop trigger if exists Person.person_Delete;
+go
+
+create trigger person_Delete on Person.PhoneNumberType
+after delete as
+	insert into Person.PhoneNumberTypeHst(Action, ModifiedDate, SourceID, UserName)
+	select 'delete', getdate(), deleted.PhoneNumberTypeID, CURRENT_USER
+	from deleted
+go
+
+/*
+ *c) Создайте представление VIEW, отображающее все поля таблицы Person.PhoneNumberType. 
+ *Сделайте невозможным просмотр исходного кода представления.
+*/
+drop view if exists Person.PView;
+go
+create view Person.PView
+with encryption
+as select * from Person.PhoneNumberType;
+go
+
+/*
+ *d) Вставьте новую строку в Person.PhoneNumberType через представление. 
+ *Обновите вставленную строку. Удалите вставленную строку. 
+ *Убедитесь, что все три операции отображены в Person.PhoneNumberTypeHst.
+*/
+insert into Person.PView (Name, ModifiedDate)
+values ('Home1', getdate());
+go
+
+update Person.PView
+set ModifiedDate = getdate()
+where ModifiedDate = (
+	select MAX(ModifiedDate) from Person.PView where Name='Home1');
+go
+
+delete from Person.PView
+where ModifiedDate = (
+	select MAX(ModifiedDate) from Person.PView where Name='Home1');
+go
---
+++ Lab_4/lab4_var6_2.sql
@@ -0,0 +1,103 @@
+﻿use AdventureWorks2012;
+go
+
+/*
+ *a) Создайте представление VIEW, отображающее данные из таблиц Person.PhoneNumberType и Person.PersonPhone. 
+ *Создайте уникальный кластерный индекс в представлении по полям PhoneNumberTypeID и BusinessEntityID.
+*/
+drop view if exists Person.PPView;
+go
+create view Person.PPView (
+	BusinessEntityID, PhoneNumber, PhoneNumberTypeID,
+	Name, ModifiedDate, TModifiedDate
+)
+with schemabinding
+as select 
+	ph.BusinessEntityID, ph.PhoneNumber, pnt.PhoneNumberTypeID,
+	pnt.Name, ph.ModifiedDate, pnt.ModifiedDate
+from Person.PersonPhone as ph
+inner join Person.PhoneNumberType as pnt on pnt.PhoneNumberTypeID = ph.PhoneNumberTypeID;
+go
+
+create unique clustered index In_PPView_TypeID_EntityID
+on Person.PPView (PhoneNumberTypeID, BusinessEntityID);
+go
+
+/*
+ *b) Создайте один INSTEAD OF триггер для представления на три операции INSERT, UPDATE, DELETE. 
+ *Триггер должен выполнять соответствующие операции в таблицах Person.PhoneNumberType и 
+ *Person.PersonPhone для указанного BusinessEntityID.
+*/
+drop trigger if exists Person.PPViewTrigger;
+go
+create trigger Person.PPViewTrigger 
+on Person.PPView
+instead of insert, update, delete as
+begin
+	if exists (select * from inserted)
+	begin
+		if not exists (select * from deleted)
+		begin
+			insert into Person.PhoneNumberType
+			select 
+				inserted.Name, GETDATE()
+			from inserted;
+
+			insert into Person.PersonPhone
+			select 
+				inserted.BusinessEntityID, inserted.PhoneNumber, 
+				pn.PhoneNumberTypeID, GETDATE()
+			from inserted
+			inner join Person.PhoneNumberType pn on pn.Name = inserted.Name;
+		end
+		else
+		begin
+			update Person.PhoneNumberType set
+				Name = inserted.Name, ModifiedDate = GETDATE()
+			from inserted, deleted
+			where Person.PhoneNumberType.PhoneNumberTypeID = deleted.PhoneNumberTypeID;
+
+			update Person.PersonPhone set
+				BusinessEntityID = inserted.BusinessEntityID, PhoneNumber = inserted.PhoneNumber,
+				ModifiedDate = GETDATE()
+			from inserted, deleted
+			where Person.PersonPhone.BusinessEntityID = deleted.BusinessEntityID;
+		end
+	end
+	else
+	begin
+		delete from Person.PersonPhone
+		where BusinessEntityID in (select BusinessEntityID from deleted)
+		and PhoneNumber in (select PhoneNumber from deleted)
+		and PhoneNumberTypeID in (select PhoneNumberTypeID from deleted);
+
+		delete from Person.PhoneNumberType
+		where PhoneNumberTypeID in (select PhoneNumberTypeID from deleted)
+	end
+end;
+go
+
+/*
+ *c) Вставьте новую строку в представление, указав новые данные для PhoneNumberType и 
+ *PersonPhone для существующего BusinessEntityID (например 1). 
+ *Триггер должен добавить новые строки в таблицы Person.PhoneNumberType и Person.PersonPhone. 
+ *Обновите вставленные строки через представление. Удалите строки.
+*/
+insert into Person.PPView 
+(BusinessEntityID, PhoneNumber, Name)
+values(1, '1', 'ddb');
+go
+
+update Person.PPView set 
+	Name = 'ddb',
+	PhoneNumber = '0'
+where PhoneNumber = '1';
+go
+
+delete from Person.PPView
+where PhoneNumber = '0';
+go
+
+select * from Person.PersonPhone;
+select * from Person.PhoneNumberType;
+go
