0583c62feab7e5108360abed66ec60bd6af30011
nnFrolovnn <mfi.ge.vlad@gmail.com>
2019-12-01 11:27:32 +0300

    lab 6, 7

---
+++ Lab_6/lab6_var6_1.sql
@@ -0,0 +1,25 @@
+﻿use AdventureWorks2012;
+go
+
+drop procedure if exists dbo.EmployeesInCities;
+go
+/*
+ *Создайте хранимую процедуру, которая будет возвращать сводную таблицу (оператор PIVOT), 
+ *отображающую данные о количестве сотрудников (HumanResources.Employee) определенного пола,
+ *проживающих в каждом городе (Person.Address). Список обозначений для пола передайте 
+ *в процедуру через входной параметр.
+*/
+create procedure dbo.EmployeesInCities(@Genders varchar(100)) as
+begin
+	execute('select City, ' + @Genders + '
+		from (
+			select e.BusinessEntityID, e.Gender, a.City
+			from HumanResources.Employee as e
+			inner join Person.BusinessEntityAddress as b on b.BusinessEntityID = e.BusinessEntityID
+			inner join Person.Address as a on a.AddressID = b.AddressID
+		) as ec
+		pivot (count(BusinessEntityID) for ec.Gender in(' + @Genders + ')) as CountOfEmployees');
+end;
+go
+
+execute dbo.EmployeesInCities 'm, f';
---
+++ Lab_7/lab7_var6_1.sql
@@ -0,0 +1,45 @@
+﻿use AdventureWorks2012;
+go 
+
+/*
+ *Вывести значения полей CreditCardID, CardType, CardNumber из таблицы Sales.CreditCard 
+ *в виде xml, сохраненного в переменную. Формат xml должен соответствовать примеру. 
+ *Создать хранимую процедуру, возвращающую таблицу, заполненную из xml переменной представленного вида. 
+ *Вызвать эту процедуру для заполненной на первом шаге переменной.
+*/
+drop procedure if exists dbo.GetCreditCards;
+go
+
+create procedure dbo.GetCreditCards(@CreditCardsXML XML)
+as
+begin
+	drop table if exists #result;
+
+    create table #result
+    (
+        CreditCardID int,
+        CardType varchar(50),
+        CardNumber varchar(25)
+    )
+
+    insert #result
+    select 
+		x.value('@ID', 'int') as CreditCardID,
+        x.value('@Type', 'varchar(50)') as CardType ,
+        x.value('@Number', 'varchar(25)') as CardNumber
+    from @CreditCardsXML.nodes('/CreditCards/Card') XmlData(x)
+
+    select * from #result;
+end;
+go
+	
+declare @xml XML;
+
+set @xml = (
+	select CreditCardID as "@ID", CardType as "@Type", CardNumber as "@Number"
+	from Sales.CreditCard
+	for xml path ('Card'), root ('CreditCards'));
+
+select @xml;
+
+exec dbo.GetCreditCards @xml;
